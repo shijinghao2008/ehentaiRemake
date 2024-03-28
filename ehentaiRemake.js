57f339e712c8f1f34f4a6f1dcfa64d8cebd5b3df
Shi <shijinghao2008@126.com>
2024-03-29 01:33:50 +1100

    更新了版本号，更新了脚本的下载和更新url、脚本名字等等基本信息。重写addStyle函数的形式来让他更加易读

--- ehentaiRemake.js
+++ ehentaiRemake.js
@@ -1,33 +1,15 @@
 // ==UserScript==
-// @name         My Custom Stylesheet
+// @name         ehentaiRemake
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Apply custom styles to a specific website
-// @author       Your Name
-// @match        http://example.com/*
+// @version      0.1
+// @description  ehentai网页端界面重做
+// @author       Shi
+// @match        https://e-hentai.org/*
+// @updateURL    https://raw.githubusercontent.com/shijinghao2008/ehentaiRemake/master/ehentaiRemake.js?token=GHSAT0AAAAAACPRXZEHEHKAYZ6BMFBLWLAEZQFP5OQ
+// @downloadURL  https://raw.githubusercontent.com/shijinghao2008/ehentaiRemake/master/ehentaiRemake.js?token=GHSAT0AAAAAACPRXZEHEHKAYZ6BMFBLWLAEZQFP5OQ
 // @grant        GM_addStyle
 // ==/UserScript==
 
-// (function() {
-//     'use strict';
-//
-//     GM_addStyle(`
-//         /* 在这里编写你的 CSS 代码 */
-//         body {
-//             font-family: Arial, sans-serif;
-//             font-size: 18px;
-//         }
-//
-//         p, li {
-//             line-height: 1.6;
-//         }
-//
-//         a {
-//             color: #0066cc;
-//         }
-//     `);
-//
-// })();
 
 function addStyle() {
     'use strict';
